7f1c6628bcd9506b2791a7d3b1b29675aaedddf4
Charles Mulder <7zh6dj9f9@mozmail.com>
2025-09-02 11:59:55 +0100

    course 2 module 2

--- src/displacement/disp.go
+++ src/displacement/disp.go
@@ -2,18 +2,32 @@ package main
 
 import (
 	"fmt"
+	"math"
 )
 
-// s = 0.5 * a * t^2 + v0 * t + s0
 func main() {
 
-	var a, v, d float64
+	// acceleration, initial velocity, initial displacement, time
+	var a, v0, s0, t float64 // time
+
 	fmt.Println("Enter acceleration:")
 	fmt.Scanln(&a)
-	fmt.Println("Enter velocity:")
-	fmt.Scanln(&v)
-	fmt.Println("Enter displacement:")
-	fmt.Scanln(&d)
+	fmt.Println("Enter initial velocity:")
+	fmt.Scanln(&v0)
+	fmt.Println("Enter initial displacement:")
+	fmt.Scanln(&s0)
+	
+	for {
+		fmt.Println("Enter time (Ctrl+C to quit):")
+		fmt.Scanln(&t)
+		Disp1 := GenDisplaceFn( a, v0, s0 )
+		fmt.Printf("Displacement is: %.2f\n\n", Disp1(t))
+	}
 
-	fmt.Printf("a: %.2f\nv: %.2f\nd: %.2f\n", a, v, d)
+}
+
+func GenDisplaceFn( a, v0, s0 float64 ) func(t float64) float64 {
+	return func(t float64) float64 {
+		return 0.5 * a * math.Pow(t, 2) + v0 * t + s0
+	}
 }
